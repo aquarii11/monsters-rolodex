4deffcfc5d41450f99976ad1051adbba754d825a
Rameel Azhungal Chellattan Rehim <rameel.rehim@alshaya.com>
2019-12-02 21:05:54 +0400

    Updates

---
+++ precache-manifest.94fbb5655d69af9efef51dfd0c00491e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd8dc7d89bc2e86df6c9710a480ec5c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cae18e98b1fa3bddbc71",
+    "url": "/monsters-rolodex/static/css/main.3cba644d.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "cae18e98b1fa3bddbc71",
+    "url": "/monsters-rolodex/static/js/main.006cc74d.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/monsters-rolodex/static/media/logo.25bf045c.svg"
+  }
+]);
